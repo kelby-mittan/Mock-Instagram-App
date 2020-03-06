ee638182413b4d8f2078ae0e90e80c13c4c52ab8
Kelby Mittan <kelbymittan@pursuit.org>
2020-03-06 13:26:43 -0500

    able to sign up

---
+++ Mock-Instagram-App/Controllers/LoginViewController.swift
@@ -0,0 +1,104 @@
+//
+//  LoginViewController.swift
+//  Mock-Instagram-App
+//
+//  Created by Kelby Mittan on 3/6/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+
+enum AccountState {
+    case existingUser
+    case newUser
+}
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var accountStateMessageLabel: UILabel!
+    @IBOutlet weak var accountStateButton: UIButton!
+    
+    private var accountState: AccountState = .existingUser
+    
+    private var authSession = AuthenticationSession()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        print("login button pressed")
+        
+        guard let email = emailTextField.text, !email.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
+            print("missing info")
+            return
+        }
+        continueLoginFlow(email: email, password: password)
+    }
+    
+    private func continueLoginFlow(email: String, password: String) {
+        if accountState == .existingUser {
+            authSession.signExistingUser(email: email, password: password) { [weak self] (result) in
+                switch result {
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        self?.showAlert(title: "Could not login", message: "\(error.localizedDescription)")
+                    }
+                case .success:
+                    DispatchQueue.main.async {
+                        self?.navigateToMainView()
+                        
+                    }
+                }
+            }
+        } else {
+            authSession.createNewUser(email: email, password: password) { [weak self] (result) in
+                
+                switch result {
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        self?.showAlert(title: "Could not sign up", message: "\(error.localizedDescription)")
+                    }
+                case .success:
+                    DispatchQueue.main.async {
+                        self?.showAlert(title: "cool", message: "you're signed up")
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    private func navigateToMainView() {
+        UIViewController.showViewController(storyboardName: "MainView", viewControllerId: "MainTabBarController")
+    }
+    
+    
+    @IBAction func toggleAccountState(_ sender: UIButton) {
+        // change the account login state
+        accountState = accountState == .existingUser ? .newUser : .existingUser
+        
+        // animation duration
+        let duration: TimeInterval = 1.0
+        
+        if accountState == .existingUser {
+            UIView.transition(with: containerView, duration: duration, options: [.transitionCrossDissolve], animations: {
+                self.loginButton.setTitle("Login", for: .normal)
+                self.accountStateMessageLabel.text = "Don't have an account ? Click"
+                self.accountStateButton.setTitle("SIGNUP", for: .normal)
+            }, completion: nil)
+        } else {
+            UIView.transition(with: containerView, duration: duration, options: [.transitionCrossDissolve], animations: {
+                self.loginButton.setTitle("Sign Up", for: .normal)
+                self.accountStateMessageLabel.text = "Already have an account ?"
+                self.accountStateButton.setTitle("LOGIN", for: .normal)
+            }, completion: nil)
+        }
+    }
+    
+}
